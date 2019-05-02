a6ec3b63f1f83bea0ceb429c2f29b60e06adf298
Devin Kapla <devinkapla@gmail.com>
2019-05-02 14:45:48 -0600

    Create post methods

--- server.js
+++ server.js
@@ -62,6 +62,39 @@ app.get('/api/v1/cities/:id/markets', (req, res) => {
     });
 });
 
+app.post('', () => {
+
+})
+
+app.post('/api/v1/cities', (req, res) => {
+  const city = req.body;
+  const errorMsg = { error: 'Expected both Name and Population of city' }
+  if (!city.population || !city.name) return res.status(422).send(errorMsg)
+
+  database('cities').insert(city, 'id')
+    .then(cities => {
+      res.status(201).json({ id: cities[0] })
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+});
+
+app.post('/api/v1/market/:id', (req, res) => {
+  const market = req.body;
+  const cityId = req.params.id
+  const errorMsg = { error: 'Expected both Name and Address of the market' }
+  if (!market.name || !market.address) return res.status(422).send(errorMsg)
+
+  database('markets').insert(market, 'id')
+    .then(market => {
+      res.status(201).json({ id: market[0] })
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+});
+
 // 4 GET endpoints ****
 // 2 POST endpoints
 // how to add market to existing city
