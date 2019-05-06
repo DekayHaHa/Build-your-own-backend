6ee14266613574084e51a814f0d3e78b553b97fe
Devin Kapla <devinkapla@gmail.com>
2019-05-06 08:35:50 -0600

    Annotate server file

--- server.js
+++ server.js
@@ -35,13 +35,17 @@ app.get('/api/v1/markets', (req, res) => {
 });
 
 app.get('/api/v1/cities/:id', (req, res) => {
+  // get request with an id param
   database('cities').where('id', req.params.id).select()
     .then(cities => {
       if (cities.length) {
+        // if the response has a value
         res.status(200).json(cities);
+        // responds with specific city according to the param id
       } else {
         res.status(404).json({
           error: `Could not find city with id ${req.params.id}`
+          // if cities.length has no value responds with 404 and error msg
         });
       }
     })
@@ -52,14 +56,17 @@ app.get('/api/v1/cities/:id', (req, res) => {
 });
 
 app.get('/api/v1/cities/markets/:id', (req, res) => {
-  //
+  // get request with an id param
   database('markets').where('id', req.params.id).select()
     .then(markets => {
       if (markets.length) {
+        // if the response has a value
         res.status(200).json(markets);
+        // responds with specific market according to the param id
       } else {
         res.status(404).json({
           error: `Could not find market with id ${req.params.id}`
+          // if markets.length has no value responds with 404 and error msg
         });
       }
     })
@@ -70,13 +77,17 @@ app.get('/api/v1/cities/markets/:id', (req, res) => {
 });
 
 app.post('/api/v1/cities', (req, res) => {
+  // post request for creating new city
   const city = req.body;
+  // assigns body to city
   const errorMsg = { error: 'Expected both Name and Population of city' }
   if (!city.population || !city.name) return res.status(422).send(errorMsg)
+  // if body does not have a value for population and name responds with 422
 
   database('cities').insert(city, 'id')
     .then(cities => {
       res.status(201).json({ id: cities[0] })
+      // responds with 201 and the id of newly created city
     })
     .catch(error => {
       res.status(500).json({ error });
@@ -85,14 +96,19 @@ app.post('/api/v1/cities', (req, res) => {
 });
 
 app.post('/api/v1/city/:id/markets', (req, res) => {
+  // post request for creating new market
   const city_id = req.params.id
+  // assigns params id to city_id for foreign id row
   const market = { ...req.body, city_id };
+  // builds new market with id & body
   const errorMsg = { error: 'Expected both Name and Address of the market' }
   if (!market.name || !market.address) return res.status(422).send(errorMsg)
+  // if body does not have a value for address and name responds with 422
 
   database('markets').insert(market, 'id')
     .then(market => {
       res.status(201).json({ id: market[0] })
+      // responds with 201 and the id of newly created market
     })
     .catch(error => {
       res.status(500).json({ error });
@@ -102,12 +118,16 @@ app.post('/api/v1/city/:id/markets', (req, res) => {
 
 
 app.delete('/api/v1/cities/markets/:id', (req, res) => {
+  // delete request for markets
   const id = req.params.id
+  // assigns specific id of market to be deleterd to id
 
   database('markets').where('id', id).del()
     .then(num => {
       if (num == 0) return res.status(404).json('No Market Found')
-      res.status(201).json(`# deleted: ${num}`)
+      // if num/response is zero/falsey responds with 404 because nothing was found
+      res.status(201).json(`Amount deleted: ${num}`)
+      // responds with 201 and amount deleted from database
     })
     .catch(error => {
       res.status(500).json({ error });
